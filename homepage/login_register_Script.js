ff303b14e8970b0ee6fd5e199c62a71f223ec1d8
Mithunvisvesh <mithunvisvesh.s@gmail.com>
2025-05-06 04:24:27 +0530

    Update login_register_Script.js

--- homepage/login_register_Script.js
+++ homepage/login_register_Script.js
@@ -43,6 +43,7 @@ function login(event) {
     
 }
 
+
 // Register function
 function register(event) {
     event.preventDefault();
@@ -50,19 +51,24 @@ function register(event) {
     const emailInput = document.getElementById("register-email");
     const passwordInput = document.getElementById("register-password");
     const confirmPasswordInput = document.getElementById("confirm-password");
+    const planInput = document.getElementById("register-plan");
     const message = document.getElementById("toggleText");
 
     const username = usernameInput.value;
     const email = emailInput.value;
     const password = passwordInput.value;
     const confirmPassword = confirmPasswordInput.value;
+    const plan = planInput.value;
+
 
     const usernameRegex = /^[a-zA-Z0-9_ ]{3,15}$/; // Allows letters, numbers, underscores, and spaces
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Simple email validation regex
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
 
+    
 
-    if (username && email && password && confirmPassword) {
+
+    if (username && email && password && confirmPassword && plan) {
         if (password !== confirmPassword) {
             message.style.color = "red";
             message.textContent = "Passwords do not match!";
@@ -79,16 +85,22 @@ function register(event) {
             message.style.color = "red";
             message.textContent = "Please enter valid password credential.";
         }
+        else if (plan !== "Freemium" && plan !== "Premium") {
+            message.style.color = "red";
+            message.textContent = "Please select a valid plan.";
+        }
         else {
             message.style.color = "green";
             message.textContent = "Registered successfully!";
+            const redirectURL = plan === "Premium" ? "payment.html" : "main.html";
             setTimeout(function() {
-                window.location.href = "main.html"; // redirect after delay
+                window.location.href = redirectURL; // redirect after delay
             }, 1500);
             usernameInput.value = "";
             emailInput.value = "";
             passwordInput.value = "";
             confirmPasswordInput.value = "";
+            planInput.value = "";
         }
     } 
     else {
@@ -96,3 +108,6 @@ function register(event) {
         message.textContent = "Please fill in all fields.";
     }
 }
+
+
+
