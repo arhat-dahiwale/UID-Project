f7bea5f0ef80d88e57d3de30d2c4c1b37c7d72f4
arhat-dahiwale <arhatdahiwale@gmail.com>
2025-10-04 11:05:05 +0530

    remove password of db

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Sicap0ll0!',
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'cinesphere_db',
   waitForConnections: true,
   connectionLimit: 10,
